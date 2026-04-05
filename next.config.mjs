3a344e91af9d345de9ab52cef4e1874e1b04c469
russel0719 <russel0719@naver.com>
2026-04-05 14:02:03 +0900

    feat: 배포 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: isProd ? '/japanese-conversation-learn' : '',
+  assetPrefix: isProd ? '/japanese-conversation-learn/' : '',
 };
 
 export default nextConfig;
